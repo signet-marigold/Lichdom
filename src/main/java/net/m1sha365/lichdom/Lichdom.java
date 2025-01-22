9d65a34986b1fd840a35f7d24e8ab25d40adb139
Alex Hack <perad@sqwu.me>
2025-01-22 00:32:39 -0600

    init

---
+++ src/main/java/net/m1sha365/lichdom/Lichdom.java
@@ -0,0 +1,16 @@
+package net.m1sha365.lichdom;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Lichdom implements ModInitializer {
+	public static final String MOD_ID = "lichdom";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/m1sha365/lichdom/LichdomClient.java
@@ -0,0 +1,13 @@
+package net.m1sha365.lichdom;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LichdomClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'onInitializeClient'");
+    }
+    
+}
