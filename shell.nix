814a78094c9a9ae68d4cd39cb22e07ea579644ff
Alex Hack <perad@sqwu.me>
2025-01-23 13:23:28 -0600

    Found new shell wrapper for libraries

--- shell.nix
+++ shell.nix
@@ -1,3 +1,5 @@
+# hmm? https://discourse.nixos.org/t/minecraft-development-environment-both-fabric-forge-unable-to-find-libraries-at-runtime/36999/2
+
 # From https://discourse.nixos.org/t/minecraft-cant-load-glx/50044/4
 # And https://discourse.nixos.org/t/issues-with-opengl-in-minecraft-development-environments/37532/5
 # Fixes error: Cannot find glfw glx: no glx : NixOS bug
@@ -9,6 +11,6 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   shellHook = ''
-    export LD_LIBRARY_PATH="''${LD_LIBRARY_PATH}''${LD_LIBRARY_PATH:+:}${pkgs.libglvnd}/lib"
+    export LD_LIBRARY_PATH="''${LD_LIBRARY_PATH}''${LD_LIBRARY_PATH:+:}${pkgs.lib.makeLibraryPath [ pkgs.libglvnd pkgs.flex ]}"
   '';
 }
