c811083d42b272c18be554e9d73109e0d16bde42
Alex Hack <perad@sqwu.me>
2025-01-27 22:31:52 -0600

    Init

--- flake.nix
+++ flake.nix
@@ -1,4 +1,6 @@
+# Minecraft dev nixos flake library loader
 # https://gist.github.com/Lgmrszd/98fb7054e63a7199f9510ba20a39bc67
+# Simply run `nix develop` on a nix system running flakes and nix command support
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
@@ -20,6 +22,12 @@
       packages = [];
       buildInputs = libs;
       LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libs;
+      shellHook = ''
+          echo "Starting runClient target"
+          ./gradlew runClient
+          echo "Exiting nix development shell"
+          exit
+        '';
     };
   };
 }
