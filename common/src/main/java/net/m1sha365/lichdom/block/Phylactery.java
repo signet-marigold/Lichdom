144552bbd7dd3c7ce5e7847fa955cd0ebb34d3b8
dan <contact@danmyers.net>
2025-01-28 18:42:14 -0600

    started working on Phylactery

---
+++ common/src/main/java/net/m1sha365/lichdom/block/Phylactery.java
@@ -0,0 +1,18 @@
+package net.m1sha365.lichdom.block;
+
+/**
+ * Item that holds a lich's essence.
+ * Very spoobky
+*/
+public class Phylactery {
+	// Needs to hold the "essence" of a "lich" player
+
+	// Method to handle respawning player at the location of the block
+	// This method needs to be public so the Lich menu can call it
+
+	// When destroyed (by anyone other than the player breaking it) then:
+	// This block needs to call the "lose all soul power method" on lich class
+
+	// Method to return player to phylactery instead of dying
+	// To be called elsewhere so make public
+}
